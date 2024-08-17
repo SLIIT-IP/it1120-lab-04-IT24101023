c0587cbb3d01d33dd48b96444816ad7a0dca0cc8
IT24101023 <IT24101023@my.sliit.lk>
2024-08-17 09:01:27 +0530

    Add files via upload
    Lab4Q1

---
+++ Q1/IT24101023Lab4Q1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class IT24101023Lab4Q1 {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+int num;
+System.out.print("Enter a number: ");
+num = input.nextInt();
+if(num>0)
+{
+System.out.print("The number is: Positive");
+}
+if(num<0)
+{
+System.out.print("The number is: Negative");
+}
+if(num==0)
+{
+System.out.print("The number is: Zero");
+}
+ 
+ 
+}
+}
